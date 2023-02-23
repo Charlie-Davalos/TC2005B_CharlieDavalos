5ffbde2a82247298e318c255a1660d5910de29bb
Charlie-Davalos <a01706041@itesm.mx>
2023-02-23 13:12:12 -0600

    new labs

--- Lab5/js/index.js
+++ Lab5/js/index.js
@@ -1,8 +1,8 @@
 // variables, constantes, consola (log, )
 
-let pulpos_mundial = "Paul";
+let pulpos_mundial = "Charls";
 
-var pulpos_boys = "Timoty";
+var pulpos_boys = "Isaac";
 
 const precio_comida_pulpos = 99.99;
 
@@ -97,6 +97,6 @@ const boton = ___("imagen");
 boton.onclick = () => {
     console.log("Click!");
     ___("contenedor_imagen").innerHTML = 
-    '<img src="https://pbs.twimg.com/media/ErpZ3_dXYAQ3BA_.jpg" alt="imagen de un pulpo enojado">';
+    '<img src="https://pbs.twimg.com/media/ErpZ3_dXYAQ3BA_.jpg" alt="angry yeah">';
 };
 
